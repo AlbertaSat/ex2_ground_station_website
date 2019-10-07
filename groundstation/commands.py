e8a89dd83e1b536a85c8f3624d9977f40b1972a7
Logan Yue <ltyue@ualberta.ca>
2019-10-07 10:32:33 -0600

    created classes for pinging the satellite and fetching housekeeping data

---
+++ groundstation/commands.py
@@ -0,0 +1,10 @@
+#all classes will extend
+class command():
+
+
+	def __init__ (self, name):
+		self.name = name
+
+	def sendCommand():
+		#TODO: call some function send() which will return info based on what is sent. At the moment, most likely just a string
+		return self.name 
