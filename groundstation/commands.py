d853752fa7ffa6ddccaefe86fcc1066b0bb82733
Logan Yue <ltyue@ualberta.ca>
2019-10-11 09:42:35 -0600

    Made turn on/off commands

--- groundstation/commands.py
+++ groundstation/commands.py
@@ -1,10 +1,79 @@
-#all classes will extend
-class command():
+#Every command will extend this. It contains a basic initialization and functions 
+#to get and set names and data as well as return the command in a tuple of the 
+#form [command type, [relevant data]] 
+class Command:
 
+	self.data = []
+	self.type = 'NONE'
 
 	def __init__ (self, name):
 		self.name = name
 
-	def sendCommand():
-		#TODO: call some function send() which will return info based on what is sent. At the moment, most likely just a string
-		return self.name 
+	#Get the formatting for send function
+	def getCommand(self):
+		return [self.type, self.data]
+
+	def getData(self):
+		return self.data
+
+	def setData(self, data):
+		self.data = data
+
+#This is the class for pinging the satellite. The data for this will be [ping id, timoeout, size]
+#TODO: consider having unique ping IDs. This might be handled in the api instead
+class Ping(Command):
+
+	self.type = 'PING'
+
+	def __init__(self, name, timeout, size):
+
+		self.name = name
+		self.timeout = timeout
+		self.size = size
+		self.data = [name, timeout, size]
+
+
+#This is the simplest command, just sending the hk type with no data necessary
+class GetHK(Command):
+
+	self.type = 'GET-HK'
+	self.data = []
+
+
+	# def __init__(self, name):
+	# 	self.name = name
+
+	def setHK(self, hk_file):
+		self.hk_file = hk_file
+
+	def processData(self):
+		#TODO read self.data and process relevant info 
+		pass
+
+class PetTimers(Command):
+
+	self.type = 'PET-TIMERS'
+
+	#timers will be an array with the name or identity of the timers that need to be pet
+	def __init__(self, name, timers):
+		self.name = name
+		self.data = timers
+
+class TurnOn(command):
+
+	self.type = 'TURN-ON'
+
+	def __init__(self, name, ID):
+		self.name = name
+		self.id = ID
+		self.data = [ID]
+
+	def getID(self):
+		return self.id
+
+	def setID(self):
+		return self.id
+
+
+
+
