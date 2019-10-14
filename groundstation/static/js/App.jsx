103d684b75d3e15aada2ac45d864645f45afae89
Farhad Makiabady <farhad.makiabady@edmonton.ca>
2019-10-14 16:21:55 -0600

    Finished initial creation of each page

--- groundstation/static/js/App.jsx
+++ groundstation/static/js/App.jsx
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 import FlightSchedule from './components/FlightSchedule';
+import Home from './components/Home'
 import { Route, Switch } from 'react-router-dom';
+import LiveCommands from './components/LiveCommands';
 
 class App extends Component{
     render() {
        return (
        		<div>
        			<Switch>
-       				<Route exact path='/' render={() => (
-		                <div>
-		                    <h1>Hello World, this is our groundstation!</h1>
-		                </div>
-		            )} />
+       				<Route exact path='/' component={Home} />
        				<Route exact path='/flightschedule' component={FlightSchedule}/>
+					<Route exact path='/livecommands' component={LiveCommands}/>
        			</Switch>
         	</div>
        )
--- groundstation/views.py
+++ groundstation/views.py
@@ -5,6 +5,7 @@ home_blueprint = Blueprint('home',__name__)
 @home_blueprint.route('/')
 @home_blueprint.route('/home')
 @home_blueprint.route('/flightschedule')
+@home_blueprint.route('/livecommands')
 def index():
 	return render_template("index.html")
 
