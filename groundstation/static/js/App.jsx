20fbcee58dea69a73925aaa7a386816a86777545
Farhad Makiabady <farhad.makiabady@edmonton.ca>
2019-10-15 14:07:39 -0600

    reorganized routes

--- groundstation/static/js/App.jsx
+++ groundstation/static/js/App.jsx
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
-import FlightSchedule from './components/FlightSchedule';
-import Home from './components/Home'
-import { Route, Switch } from 'react-router-dom';
-import LiveCommands from './components/LiveCommands';
+import Routes from './routes'
+import './App.css';
 
 class App extends Component{
     render() {
        return (
        		<div>
-       			<Switch>
-       				<Route exact path='/' component={Home} />
-       				<Route exact path='/flightschedule' component={FlightSchedule}/>
-					<Route exact path='/livecommands' component={LiveCommands}/>
-       			</Switch>
+				<Routes />
         	</div>
        )
     }
