fed419163b6a8474e554d4c573fbaed4f9857447
Farhad Makiabady <farhad.makiabady@edmonton.ca>
2019-10-15 21:36:57 -0600

    changed app into a function instead of class

--- groundstation/static/js/App.jsx
+++ groundstation/static/js/App.jsx
@@ -2,15 +2,12 @@ import React, { Component } from 'react';
 import Routes from './routes'
 import './App.css';
 
-class App extends Component{
-    render() {
-       return (
+function App() {
+    return (
        		<div>
 				<Routes />
         	</div>
-       )
+       );
     }
-
-}
 
 export default App;
