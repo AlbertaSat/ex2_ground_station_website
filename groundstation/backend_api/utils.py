ba57a4954a47ba4706ae271044945b44455e4336
Tyler <tlazaren@ualberta.ca>
2019-10-18 22:54:32 -0600

    added quick explanation

--- groundstation/backend_api/utils.py
+++ groundstation/backend_api/utils.py
@@ -2,6 +2,9 @@ from flask import has_app_context
 from groundstation import create_app
 
 
+# a decorator to handle cases where backend api calls have no app context
+# namely for when locally using the api, db session must be initiated 
+# in order to access models and database operation
 def create_context(function):
     def decorate(*args, **kwargs):
         if not has_app_context():
@@ -14,5 +17,3 @@ def create_context(function):
              return function(args, **kwargs) 
 
     return decorate
-
-
