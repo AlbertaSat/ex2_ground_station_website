2a1dd2a40b907e12350b9c47419fa7b7773072e1
Farhad Makiabady <farhad.makiabady@edmonton.ca>
2019-10-19 17:25:45 -0600

    fetched housekeeping data from api

--- groundstation/static/js/components/Home.jsx
+++ groundstation/static/js/components/Home.jsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Home = () => (
-  <div>
-    <h1>This is our GroundStation!</h1>
-  </div>
-)
+class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoading: false,
+      housekeeping: {
+        id: null,
+        satelliteMode: null,
+        batteryVoltage: null,
+        currentIn: null,
+        currentOut: null,
+        lastBeaconTime: null,
+        noMCUResets: null
+      }
+    };
+  }
 
+  componentDidMount() {
+    this.state.isLoading = true;
+    fetch('/api/housekeepinglog/1')
+    .then(results => {
+      return results.json();
+    }).then(data => {
+      this.state.isLoading = false;
+      this.state.housekeeping.id = data.data.id;
+      this.state.housekeeping.satelliteMode = data.data.satelliteMode;
+      this.state.housekeeping.batteryVoltage = data.data.batteryVoltage;
+      this.state.housekeeping.currentIn = data.data.currentIn;
+      this.state.housekeeping.currentOut = data.data.currentOut;
+      this.state.housekeeping.lastBeaconTime = data.data.lastBeaconTime;
+      this.state.housekeeping.noMCUResets = data.data.noMCUResets;
+      console.log(this.state.housekeeping);
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>This is our GroundStation!</h1>
+      </div>
+    )
+  }
+}
 export default Home;
