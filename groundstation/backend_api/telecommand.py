db3cf5b75624dbf370ae97de61804f0a89bb655b
Logan Yue <ltyue@ualberta.ca>
2019-10-27 17:10:53 -0600

    filled in most of the telecommand function however still need to consult others on the formatting

--- groundstation/backend_api/telecommand.py
+++ groundstation/backend_api/telecommand.py
@@ -12,10 +12,50 @@ from flask_restful import Resource, Api
 from datetime import datetime
 import json
 
-from groundstation.backend_api.models import Housekeeping
+from groundstation.backend_api.models import telecommand
 from groundstation import db
-from groundstation.backend_api.utils import create_context
+from groundstation.backend_api.utils import create_context, add_telecommand
 
 telecommand_blueprint = Blueprint('telecommand', __name__)
 api = Api(telecommand_blueprint)
+
+class Telecommand(Resource):
+
+	@create_context
+    def post(self, local_data=None):
+        
+        """Post a telecommand"""
+        # this api call will have to treat incoming data different if it is called locally
+        response_object = {
+            'status': 'fail',
+            'message': 'Invalid payload'
+        }
+
+        if not local_data:
+            post_data = request.get_json()
+        else:
+            post_data = json.loads(local_data)
+
+        try:
+            #post_data['lastBeaconTime'] = datetime.strptime(post_data['lastBeaconTime'], '%Y-%m-%d %H:%M:%S')
+            #I don't know the format or how to pull data from the JSON so will consult others and fill in this section
+            #TODO: reformat or extract data from JSON
+        except (ValueError, TypeError, KeyError) as error:
+            return response_object, 400
+
+        telecommand = Telecommands(**post_data)
+        db.session.add(telecommand)
+        db.session.commit()
+
+        response_object = {
+            'status': 'success',
+            #not sure on the formatting for this yet, need to consult others
+            'message': 'Command {command.name} was added!'
+        }
+
+        return response_object, 201
+
+
+
+        return response_object, 200
 
