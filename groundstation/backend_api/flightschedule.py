517a43b5ec6fc5ac96fc13f3d3bb6b211391ca5b
Nicholas Whitefield <whitefie@ualberta.ca>
2019-10-26 19:06:19 -0600

    stated endpoint

--- groundstation/backend_api/flightschedule.py
+++ groundstation/backend_api/flightschedule.py
@@ -1,8 +1,27 @@
 from flask import Blueprint
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 
 from groundstation import db
 from groundstation.backend_api.utils import create_context
 
 flightschedule_blueprint = Blueprint('flightschedule', __name__)
 api = Api(flightschedule_blueprint)
+
+class FlightScheduleList(Resource):
+
+    def __init__(self):
+        self.parser = reqparse.RequestParser()
+        self.parser.add_argument('rate', type=int, help='Rate to charge for this resource')
+
+        # commands = db.relationship('FlightScheduleCommands', backref='flightschedule', lazy=True)
+
+
+    def get(self):
+        pass
+
+    def post(self):
+        args = parser.parse_args()
+
+
+
+api.add_resource(FlightScheduleList, '/api/flightschedules')
