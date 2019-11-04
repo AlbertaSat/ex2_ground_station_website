f0dd95b0492828c16eea16253724ffaea0c7f763
Farhad Makiabady <farhad.makiabady@edmonton.ca>
2019-11-03 21:03:37 -0700

    show housekeeping logs with a full screen modal

--- groundstation/static/js/components/Housekeeping.jsx
+++ groundstation/static/js/components/Housekeeping.jsx
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
+import HousekeepingList from './HousekeepingListFull';
 
 class HouseKeeping extends Component {
-
+    constructor() {
+        super();
+        this.state = {
+          empty: true,
+          isLoaded: false,
+          housekeeping: {
+            id: null,
+            satelliteMode: null,
+            batteryVoltage: null,
+            currentIn: null,
+            currentOut: null,
+            lastBeaconTime: null,
+            noMCUResets: null
+          },
+          flightschedule: []
+        };
+      }
+    
+      componentDidMount() {
+        fetch('/api/housekeepinglog')
+        .then(results => {
+         return results.json();
+        }).then(data => {
+         console.log('data: ', data);
+         if (data.status == 'success') {
+           this.setState({ housekeeping: data.data.logs, isLoaded: true, empty: false })
+           console.log(this.state.housekeeping);
+         }
+        });
+      }
 
     render() {
         return (
             <div>
-                <h1>housekeeping</h1>
+                <h1 style={{padding: '10px'}}>HouseKeeping</h1>
+                <HousekeepingList housekeeping={this.state.housekeeping} empty={this.state.empty} />
             </div>
         )
     }
