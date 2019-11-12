3e48e9f9f11d4c54a8f40fbc457800b688cc574b
Logan Yue <ltyue@ualberta.ca>
2019-11-11 17:45:21 -0700

    started styling the login page

--- groundstation/static/js/components/Login.jsx
+++ groundstation/static/js/components/Login.jsx
@@ -1,13 +1,51 @@
 import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+// const Login = () => (
+//   <div>
+//     <h1>This is our Login test</h1>
+//   </div>
+// )
 
-const Login = () => (
-  <div>
-    <h1>This is our Login</h1>
-  </div>
-)
+const styles = {
+  root: {
+    padding: '42px',
+    display: 'inline-flex',
+    alignItems: 'center'
+  },
+};
 
-// class Login extends Component {
 
-// }
+class Login extends Component {
+    constructor(){
+        super();
+        this.state = {
 
-export default Login;
+        }
+
+    };
+
+    // ComponentDidMount(){
+
+    // }
+
+    render(){
+        const { classes } = this.props;
+        return (
+            <div>
+                <Grid container spacing={2} alignItems='flex-end'>
+                    <Grid item sm={8}>
+                        <div className={classes.root}>
+                            <Typography variant="h4" displayInline style={{color: '#28324C'}}>
+                                Login
+                            </Typography>
+                        </div>
+                    </Grid>
+                </Grid>
+            </div>
+        )
+    }
+}
+
+export default withStyles(styles)(Login);
