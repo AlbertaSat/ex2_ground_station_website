82fd33d0c3bd8c5ea036454e3e6dbde1496fed11
Logan Yue <ltyue@ualberta.ca>
2019-11-13 13:31:14 -0700

    created a basic login visual

--- groundstation/static/js/components/Login.jsx
+++ groundstation/static/js/components/Login.jsx
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+// import Visibility from '@material-ui/icons/Visibility';
+// import VisibilityOff from '@material-ui/icons/VisibilityOff';
 // const Login = () => (
 //   <div>
 //     <h1>This is our Login test</h1>
@@ -29,18 +32,45 @@ class Login extends Component {
     // ComponentDidMount(){
 
     // }
+    // handleLogin(event){
+    //     event.preventDefault();
+        
+    // }
 
     render(){
         const { classes } = this.props;
         return (
             <div>
                 <Grid container spacing={2} alignItems='flex-end'>
-                    <Grid item sm={8}>
+                    <Grid item xs={12}>
                         <div className={classes.root}>
                             <Typography variant="h4" displayInline style={{color: '#28324C'}}>
                                 Login
                             </Typography>
                         </div>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            required
+                            id="outlined-required"
+                            label="Email"
+                            autoComplete="example@email.com"
+                            className={classes.textField}
+                            margin="normal"
+                            variant="outlined"
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            required
+                            id="outlined-password-input"
+                            label="Password"
+                            className={classes.textField}
+                            type="password"
+                            autoComplete="current-password"
+                            margin="normal"
+                            variant="outlined"
+                        />
                     </Grid>
                 </Grid>
             </div>
