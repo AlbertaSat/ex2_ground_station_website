f1e59e8b6dc5870a7d18c2573040ef30ba30e253
hughbagan <hbagan@ualberta.ca>
2019-11-21 14:55:07 -0700

    Set LiveCommands input text field width to 100%

--- groundstation/static/js/components/LiveCommands.jsx
+++ groundstation/static/js/components/LiveCommands.jsx
@@ -205,7 +205,7 @@ class LiveCommands extends Component {
                       label="Enter Telecommand"
                       margin="normal"
                       variant="outlined"
-                      style={{width:"30%","background-color":"white"}}
+                      style={{width:"100%","background-color":"white"}}
                       value={this.state.textBoxValue}
                       onChange={(event) => this.handleChange(event)}
                       onKeyDown={(event) => this.handleKeyPress(event) }
