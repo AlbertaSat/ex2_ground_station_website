c55a8402ca04814bb2cc49a0380f4c4d2ab1da86
Student <ldmcdona@ualberta.ca>
2019-11-25 12:30:16 -0500

    initial commit

--- manage.py
+++ manage.py
@@ -97,6 +97,63 @@ def seed_db():
 
 
 
+@cli.command('demo_db')
+def demo_db():
+    timestamp = datetime.fromtimestamp(1570749472)
+    housekeepingData = fakeHousekeepingAsDict(timestamp)
+
+    housekeeping = Housekeeping(**housekeepingData)
+    db.session.add(housekeeping)
+    db.session.commit()
+
+    commands = {
+        'ping': (0,False),
+        'get-hk':(0,False),
+        'turn-on':(1,True),
+        'turn-off':(1,True),
+        'set-fs':(1,True),
+        'upload-fs': (0, False)
+    }
+
+    for name, (num_args, is_danger) in commands.items():
+        c = add_telecommand(command_name=name, num_arguments=num_args, is_dangerous=is_danger)
+
+    command = Telecommands.query.filter_by(command_name='ping').first()
+    flightschedule = add_flight_schedule(creation_date=timestamp, upload_date=timestamp, status=2)
+    flightschedule_commands = add_command_to_flightschedule(
+                                timestamp=timestamp,
+                                flightschedule_id=flightschedule.id,
+                                command_id=command.id
+                            )
+
+    add_user(username='Admin_user', password='Admin_user', is_admin=True)
+    add_user(username='user1', password='user1', is_admin=False)
+    add_user(username='user2', password='user2', is_admin=False)
+    command = Telecommands.query.filter_by(command_name='turn-on').first()
+    flightschedule_commands = add_command_to_flightschedule(
+                                timestamp=timestamp,
+                                flightschedule_id=flightschedule.id,
+                                command_id=command.id
+                            )
+
+    flightschedulecommand_arg = add_arg_to_flightschedulecommand(
+                                index=0,
+                                argument='5',
+                                flightschedule_command_id=flightschedule_commands.id
+                            )
+
+    message = add_message_to_communications(
+                    timestamp=timestamp,
+                    message='ping',
+                    sender='user',
+                    receiver='comm'
+                )
+
+    now = datetime.utcnow()
+    add_passover(timestamp=now - timedelta(seconds=10))
+    for i in range(5):
+        add_passover(timestamp=now + timedelta(minutes=i*10))
+
 
 
 
