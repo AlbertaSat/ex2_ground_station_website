d2de6e544599eadab87228433c46bde57be7acb3
Student <ldmcdona@ualberta.ca>
2019-11-25 13:12:08 -0500

    Just need to add more flight schedules?

--- manage.py
+++ manage.py
@@ -101,25 +101,33 @@ def seed_db():
 def demo_db():
     timestamp = datetime.fromtimestamp(1570749472)
     time2 = datetime.fromisoformat('2019-11-04 00:05:23.283+00:00')
-    time3 = datetime.fromisoformat('2019-11-05 00:08:43.203+00:00')
-    time4 = datetime.fromisoformat('2019-11-05 00:15:20.118+00:00')
+    #time3 = datetime.fromisoformat('2019-11-05 00:08:43.203+00:00')
+    #time4 = datetime.fromisoformat('2019-11-05 00:15:20.118+00:00')
 
     housekeepingData = fakeHousekeepingAsDict(timestamp)
     hkd2 = fakeHousekeepingAsDict(time2)
-    hkd3 = fakeHousekeepingAsDict(time3)
-    hkd4 = fakeHousekeepingAsDict(time4)
+    #hkd3 = fakeHousekeepingAsDict(time3)
+    #hkd4 = fakeHousekeepingAsDict(time4)
 
     housekeeping = Housekeeping(**housekeepingData)
     hk2 = Housekeeping(**hkd2)
-    hk3 = Housekeeping(**hkd3)
-    hk4 = Housekeeping(**hkd4)
+    #hk3 = Housekeeping(**hkd3)
+    #hk4 = Housekeeping(**hkd4)
 
     db.session.add(housekeeping)
     db.session.add(hk2)
-    db.session.add(hk3)
-    db.session.add(hk4)
-    
+    #db.session.add(hk3)
+    #db.session.add(hk4)
+
     db.session.commit()
+
+    add_user(username='Ex-Alta-Master', password='master88', is_admin=True)
+    add_user(username='Ex-Alta-Safety', password='totalRecall', is_admin=True)
+    add_user(username='Manny1996', password='valientHearts', is_admin=False)
+    add_user(username='Impeach45', password='russia45', is_admin=False)
+    add_user(username='StarFinder', password='spaceForce', is_admin=False)
+    add_user(username='JojoBagins', password='ExOhExOhEx', is_admin=False)
+    add_user(username='HJDewit', password='simplify0923', is_admin=False)
 
     commands = {
         'ping': (0,False),
@@ -141,14 +149,6 @@ def demo_db():
                                 command_id=command.id
                             )
 
-    add_user(username='Ex-Alta-Master', password='master88', is_admin=True)
-    add_user(username='Ex-Alta-Safety', password='totalRecall', is_admin=True)
-    add_user(username='Manny1996', password='valientHearts', is_admin=False)
-    add_user(username='Impeach45', password='russia45', is_admin=False)
-    add_user(username='StarFinder', password='spaceForce', is_admin=False)
-    add_user(username='JojoBagins', password='ExOhExOhEx', is_admin=False)
-    add_user(username='HJDewit', password='simplify0923', is_admin=False)
-
     command = Telecommands.query.filter_by(command_name='turn-on').first()
     flightschedule_commands = add_command_to_flightschedule(
                                 timestamp=timestamp,
@@ -164,6 +164,27 @@ def demo_db():
 
     message = add_message_to_communications(
                     timestamp=timestamp,
+                    message='ping',
+                    sender='user',
+                    receiver='comm'
+                )
+
+    command = Telecommands.query.filter_by(command_name='ping').first()
+    flightschedule = add_flight_schedule(creation_date=time2, upload_date=time2, status=2)
+    flightschedule_commands = add_command_to_flightschedule(
+                                timestamp=time2,
+                                flightschedule_id=flightschedule.id,
+                                command_id=command.id
+                            )
+
+    flightschedulecommand_arg = add_arg_to_flightschedulecommand(
+                                index=1,
+                                argument='5',
+                                flightschedule_command_id=flightschedule_commands.id
+                            )
+
+    message = add_message_to_communications(
+                    timestamp=time2,
                     message='ping',
                     sender='user',
                     receiver='comm'
