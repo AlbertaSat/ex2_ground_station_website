92b28fe1473114e6fec351ad6a6f762fb3ed55f5
Tyler <tlazaren@ualberta.ca>
2019-11-25 17:50:39 -0700

    you can now view passovers on the homepage

---
+++ groundstation/static/js/components/Passovers.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+
+import LinearProgress from '@material-ui/core/LinearProgress';
+import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+  },
+  title: {
+    marginLeft: theme.spacing(2),
+    flex: 1,
+  },
+  paper: {
+    marginTop: theme.spacing(3),
+    width: '100%',
+    overflowX: 'auto',
+    marginBottom: theme.spacing(2),
+  },
+}));
+
+const Passovers = (props) => {
+	if (props.isLoading) {
+      return (
+        <div>
+          <LinearProgress />
+        </div>
+      )
+	}
+
+  if (props.empty && !props.isLoading) {
+    return (
+      <div>
+        <ErrorOutlineIcon /> There is currently no housekeeping data!
+      </div>
+    )
+  }
+
+  const classes = useStyles();
+
+  return (
+  	<div className={classes.root}>
+  	  <Paper className={classes.paper}>
+  	    {props.passovers.map(passover => (
+	  	   <Table aria-label="simple table">
+		  	  <TableBody>
+		        <TableRow key={passover.id}>
+		           <TableCell width="15%" component="th" scope="row">
+		              {passover.timestamp.split('.')[0]}
+		           </TableCell>
+		        </TableRow>
+		      </TableBody>
+	       </Table>
+    	))}
+  	  </Paper>
+  	</div>
+  )
+
+}
+
+export default Passovers;
