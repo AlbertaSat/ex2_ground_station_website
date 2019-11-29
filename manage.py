daa5fbb1ddbd89c76028d3444ff8ae755531a13a
Logan Yue <ltyue@ualberta.ca>
2019-11-29 11:58:03 -0700

    added albert and berta users

--- manage.py
+++ manage.py
@@ -99,6 +99,9 @@ def seed_db():
     add_user(username='Admin_user', password='Admin_user', is_admin=True)
     add_user(username='user1', password='user1', is_admin=False)
     add_user(username='user2', password='user2', is_admin=False)
+    add_user(username='albert', password='albert', is_admin=False)
+    add_user(username='berta', password='berta', is_admin=True)
+
     command = Telecommands.query.filter_by(command_name='turn-on').first()
     flightschedule_commands = add_command_to_flightschedule(
                                 timestamp=timestamp,
