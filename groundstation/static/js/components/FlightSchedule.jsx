a97aa9476c2cea26a60b6d15db0ed587eead1077
Farhad Makiabady <farhad.makiabady@edmonton.ca>
2019-11-29 11:21:00 -0700

    removed some more console logs

--- groundstation/static/js/components/FlightSchedule.jsx
+++ groundstation/static/js/components/FlightSchedule.jsx
@@ -146,8 +146,6 @@ class FlightSchedule extends Component{
 		}).then(results => {
 			return results.json();
 		}).then(data => {
-			console.log(data)
-
 			if(data.status == 'success'){
 				const obj = this.state.allflightschedules.slice();
 				// depending on what we are doing, we handle the resulting data differently
@@ -210,7 +208,6 @@ class FlightSchedule extends Component{
 		// however it is newly created, so deleting it shouldnt be
 		// patched to the database
 		this.setState({thisFlightscheduleCommands: obj})
-		console.log(this.state.thisFlightscheduleCommands);
 	}
 
 	handleExecutionTimeChange(event){
