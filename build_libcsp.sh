355f9a457b1f9530bb7c0e36fd26f61712ef544b
John M <johnmabanta51@gmail.com>
2022-03-06 19:49:54 -0700

    Added script to build libcsp submodule

---
+++ build_libcsp.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+# For Ubuntu
+
+# Fetch submodules
+git submodules update --init --recursive
+
+# Fetch libcsp dependencies
+# NOTE: Python is required but not installed here as to not interfere
+# with the setup-python Github Action
+sudo apt install at \
+    build-essential \
+    wget \
+    curl \
+    libpq-dev \
+    libzmq3-dev \
+    libsocketcan-dev \
+    pkg-config \
+    gcc-multilib \
+    g++-multilib -y
+
+# Configure libcsp
+cd libcsp
+python3 waf configure --with-os=posix --enable-can-socketcan --enable-rdp --enable-crc32 --enable-hmac --enable-xtea --with-loglevel=debug --enable-debug-timestamp --enable-python3-bindings --with-driver-usart=linux --enable-if-zmqhub --enable-examples
+python3 waf build
+cd ..
