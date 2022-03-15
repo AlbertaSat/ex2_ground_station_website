a2e0b9f85ac0058f793bdbdcb3c991735642b315
ahmedsiad <ahmedsiadsemail@gmail.com>
2022-03-15 14:16:41 -0600

    Add AOS detection to automation.py + Slack notifs

--- automation.py
+++ automation.py
@@ -1,8 +1,13 @@
 """The Automation Module allows operators to execute commands automatically at the next pass.
 """
-from groundstation.backend_api.communications import CommunicationList
+from groundstation.backend_api.communications import CommunicationList, Communication
+from groundstation.backend_api.housekeeping import HousekeepingLogList
 from groundstation.backend_api.passover import PassoverList
+from groundstation.backend_api.utils import add_passover
+from groundstation.backend_api.user import UserList
 from datetime import datetime, timezone
+from pyorbital.orbital import Orbital
+import slack
 import json
 import subprocess
 import time
@@ -36,15 +41,20 @@ def automate_passovers():
     be automatically run again during the next pass.
     """
     passover = PassoverList()
+    housekeeping = HousekeepingLogList()
 
     # the automation will also handle queuing passover times
     passovers = passover.get(local_args={'limit': 1, 'next': 'true'})
+    last_pass = passover.get(local_args={'most-recent': 'true'})[0]['data']['most_recent_passover']
+
+    message = 'An Ex-Alta 2 passover is beginning now! The timestamp for this passover is {0}'.format(last_pass['timestamp'])
+    send_slack_notifs(message)
 
     if passovers[1] == 200:
         passover_data = passovers[0]['data']['next_passovers']
-        for passover in passover_data:
+        for ps in passover_data:
             time_obj = datetime.strptime(
-                passover['timestamp'], '%Y-%m-%d %H:%M:%S.%f')
+                ps['timestamp'], '%Y-%m-%d %H:%M:%S.%f')
             time_obj = time_obj.replace(
                 tzinfo=timezone.utc).astimezone(tz=None)
             f_time_min = time_obj.strftime('%H:%M')
@@ -55,6 +65,35 @@ def automate_passovers():
             print("Scheduled to automate at the next passover.")
     else:
         print("AUTOMATION: no more passovers found.")
+        # calculate new passovers for the next 24 hours using current TLE data
+
+        hk = housekeeping.get(local_args={'limit': 1, 'newest-first': 'true'})
+
+        tle = hk[0]['data']['logs'][0]['tle']
+        lines = tle.split('\n')
+    
+        orb = Orbital('ex-alta 2', line1=lines[0], line2=lines[1])
+        dtobj = datetime.utcnow()
+        passes = orb.get_next_passes(dtobj, 24, -113.4938, 53.5461, 0.645) # edmonton coordinates and elevation
+        ps_data = {'passovers': [{'timestamp': str(ps[0])} for ps in passes]}
+
+        passover.post(local_data=json.dumps(ps_data))
+        
+
+def send_slack_notifs(message):
+    # api call to get all users
+    user_list = UserList()
+
+    users = user_list.get(local_args={'limit': 1000})[0]['data']
+
+    client = slack.WebClient(token='xoxb-58810056594-3097149250131-xK3JH7caXxC5oQ9aQ0etzAsM')
+    for user in users:
+        if user.subscribed_to_slack and user.slack_id is not None:
+            try:
+                client.chat_postMessage(channel=user.slack_id, text=message)
+            except:
+                print('Error: slack id "{0}" is invalid.'.format(user.slack_id))
+
 
 
 def main():
