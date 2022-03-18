48af88376586f69c0f591b0c0e34859993859b22
John M <johnmabanta51@gmail.com>
2022-03-17 18:01:36 -0600

    Improved libcsp build script

--- build_libcsp.sh
+++ build_libcsp.sh
@@ -7,6 +7,7 @@ git submodule update --init --recursive
 # Fetch libcsp dependencies
 # NOTE: Python is required but not installed here as to not interfere
 # with the setup-python Github Action
+sudo apt update
 sudo apt install at \
     build-essential \
     wget \
@@ -19,6 +20,7 @@ sudo apt install at \
     g++-multilib -y
 
 # Configure libcsp
+# (Configuration flags taken from ex2_ground_station_software)
 cd libcsp
 python3 waf configure --with-os=posix --enable-can-socketcan --enable-rdp --enable-crc32 --enable-hmac --enable-xtea --with-loglevel=debug --enable-debug-timestamp --enable-python3-bindings --with-driver-usart=linux --enable-if-zmqhub --enable-examples
 python3 waf build
