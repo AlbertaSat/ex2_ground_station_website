9a0e654531c03ba930a72b71d5f1a0848cd229ad
John M <johnmabanta51@gmail.com>
2022-04-04 18:11:24 -0600

    Added dropdown to select repeat settings

--- groundstation/static/js/components/FlightscheduleCommand.jsx
+++ groundstation/static/js/components/FlightscheduleCommand.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableRow from "@material-ui/core/TableRow";
@@ -6,9 +6,48 @@ import TextField from "@material-ui/core/TextField";
 import DeleteIcon from "@material-ui/icons/Delete";
 import Button from "@material-ui/core/Button";
 import Select from "react-select";
+import Popover from "@material-ui/core/Popover";
 import { makeStyles } from "@material-ui/core/styles";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+
+const RepeatLabels = {
+  repeat_sec: "...Second",
+  repeat_min: "...Minute",
+  repeat_hr: "...Hour",
+  repeat_wkday: "...Weekday",
+  repeat_day: "...Day",
+  repeat_month: "...Month",
+  repeat_year: "...Year",
+};
 
 const FlightscheduleCommand = (props) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [repeats, setRepeats] = useState({
+    repeat_sec: false,
+    repeat_min: false,
+    repeat_hr: false,
+    repeat_wkday: false,
+    repeat_day: false,
+    repeat_month: false,
+    repeat_year: false,
+  });
+
+  const handleOpenRepeat = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
+
+  const handleCloseRepeat = (e) => {
+    setAnchorEl(null);
+  };
+
+  const updateRepeat = (field, value) => {
+    setRepeats((old) => {
+      return { ...old, [field]: value };
+    });
+  };
+
   const selects = props.availCommands.map((command) => ({
     label: command.command_name,
     value: command.command_id,
@@ -37,11 +76,15 @@ const FlightscheduleCommand = (props) => {
     }
   }
 
+  const popoverOpen = Boolean(anchorEl);
+
   return (
     <TableBody>
       <TableRow>
         <TableCell
-          className={props.flightschedule.args.length > 0 ? classes.argBottom : null}
+          className={
+            props.flightschedule.args.length > 0 ? classes.argBottom : null
+          }
           style={{ minWidth: "18em" }}
         >
           <form>
@@ -58,7 +101,9 @@ const FlightscheduleCommand = (props) => {
                   padding: "10px 10px",
                 }),
               }}
-              onChange={(event) => props.handleAddEvent(event, "command", props.idx)}
+              onChange={(event) =>
+                props.handleAddEvent(event, "command", props.idx)
+              }
               value={{
                 label: props.flightschedule.command.command_name,
                 value: props.flightschedule.command.command_id,
@@ -67,7 +112,9 @@ const FlightscheduleCommand = (props) => {
           </form>
         </TableCell>
         <TableCell
-          className={props.flightschedule.args.length > 0 ? classes.argBottom : null}
+          className={
+            props.flightschedule.args.length > 0 ? classes.argBottom : null
+          }
         >
           <form>
             <TextField
@@ -79,22 +126,60 @@ const FlightscheduleCommand = (props) => {
                 props.flightschedule.timestamp,
                 props.executionTime
               )}
-              onChange={(event) => props.handleAddEvent(event, "date", props.idx)}
+              onChange={(event) =>
+                props.handleAddEvent(event, "date", props.idx)
+              }
             />
           </form>
         </TableCell>
         <TableCell
-          className={props.flightschedule.args.length > 0 ? classes.argBottom : null}
+          className={
+            props.flightschedule.args.length > 0 ? classes.argBottom : null
+          }
         >
           <form>
-            <Button>Repeat Settings</Button>
+            <Button onClick={handleOpenRepeat}>Repeat Every...</Button>
+            <Popover
+              open={popoverOpen}
+              anchorEl={anchorEl}
+              onClose={handleCloseRepeat}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "center",
+              }}
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "center",
+              }}
+            >
+              <FormGroup>
+                {Object.keys(repeats).map((field, idx) => (
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        checked={repeats[field]}
+                        onChange={(e) => {
+                          updateRepeat(field, e.target.checked);
+                        }}
+                      />
+                    }
+                    label={RepeatLabels[field]}
+                    key={idx}
+                  />
+                ))}
+              </FormGroup>
+            </Popover>
           </form>
         </TableCell>
         <TableCell
-          className={props.flightschedule.args.length > 0 ? classes.argBottom : null}
+          className={
+            props.flightschedule.args.length > 0 ? classes.argBottom : null
+          }
         >
           <Button
-            onClick={(event) => props.handleDeleteCommandClick(event, props.idx)}
+            onClick={(event) =>
+              props.handleDeleteCommandClick(event, props.idx)
+            }
           >
             <DeleteIcon style={{ color: "#4bacb8" }} />
           </Button>
