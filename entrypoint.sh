521471ca0413ade067c92daa12bb073980e0d4f0
John M <johnmabanta51@gmail.com>
2022-05-24 14:45:53 -0600

    nginx web server now works in docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 # This script is for the entrypoint when the app is deployed on a server
 pip install -r requirements.txt
-python3 manage.py recreate_db
-python3 manage.py seed_db
+export FLASK_ENV=production
+export APP_SETTINGS=groundstation.config.ProductionConfig
+cd groundstation/static
+npm install
+npm run build
+cd ../..
+# python3 manage.py recreate_db
+# python3 manage.py seed_db
 
 gunicorn -b 0.0.0.0:5000 manage:app
