50c2cee4333ea98e0db6720d4a265a181a05c4da
ahmedsiad <ahmedsiadsemail@gmail.com>
2022-05-30 18:55:27 -0600

    Remember me functionality

--- groundstation/static/js/components/Logout.jsx
+++ groundstation/static/js/components/Logout.jsx
@@ -3,6 +3,17 @@ import { Redirect } from 'react-router-dom';
 
 class Logout extends Component{
 	componentDidMount() {
+		fetch('/api/auth/logout', {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				'Authorization': 'Bearer ' + sessionStorage.getItem('auth_token')
+			},
+			keepalive: true
+		});
+
+		sessionStorage.clear('auth_token');
+		sessionStorage.clear('username');
 		localStorage.clear('auth_token');
 		localStorage.clear('username');
   	};
