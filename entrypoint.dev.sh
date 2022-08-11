b8379727d9fa264646e6236141885cbc3c6fa507
John Mabanta <johnmabanta51@gmail.com>
2022-08-11 09:07:54 -0600

    pip now updates itself before install python dependencies

--- entrypoint.dev.sh
+++ entrypoint.dev.sh
@@ -3,9 +3,16 @@
 # Setup autoformat hook
 git config core.hooksPath .git-hooks/hooks/
 
+# Update pip and install python dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
+
+# Setup environment variables
 source ./env.sh
 source ./keys.sh
+
+# Setup example database
 python3 manage.py recreate_db
 python3 manage.py seed_db_example
+
 exec "$@"
